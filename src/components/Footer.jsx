343a521e9136e3ac9abbf7a871038f71d714116c
0SCAR0421 <oscar0421@kakao.com>
2023-06-26 15:17:37 +0900

    Feat: Footer 구현

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,27 @@
+import styled from 'styled-components';
+
+const FooterContainer = styled.footer`
+  width: 100vw;
+  height: 200px;
+  padding: 30px;
+
+  & > a {
+    color: #000;
+  }
+
+  & > p {
+    margin-top: 5px;
+    font-size: 12px;
+  }
+`;
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <FooterContainer>
+      <a href="https://github.com/wj-react-team27/movie-app">Github</a>
+      <p>Copyright Ⓒ 2023. Team 27. All rights reserved.</p>
+    </FooterContainer>
+  );
 };
 
 export default Footer;
