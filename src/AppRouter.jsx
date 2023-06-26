ab0f6dea3e68c0b839964a511b951cc168bf7f34
0SCAR0421 <oscar0421@kakao.com>
2023-06-26 15:49:14 +0900

    Feat: 404 Page 작성

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -3,6 +3,7 @@ import MainPage from './pages/MainPage';
 import DetailPage from './pages/DetailPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import NotFound from './components/NotFound';
 
 const AppRouter = () => {
   return (
@@ -11,7 +12,7 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/detail/:id" element={<DetailPage />} />
-        <Route path="/*" element={<div>404 NOT FOUND</div>} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
       <Footer />
     </BrowserRouter>
