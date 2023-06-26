4e759cefa51f89f300788cd3e719d2876bab0d1f
0SCAR0421 <oscar0421@kakao.com>
2023-06-26 15:18:08 +0900

    Feat: Loading 구현

---
+++ src/components/Loading.jsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+const LoadingContainer = styled.div`
+  width: 100vw;
+  height: 500px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const LoadingCircleContainer = styled.div`
+  @keyframes zoom-in-zoom-out {
+    0% {
+      transform: scale(1);
+    }
+    50% {
+      transform: scale(1.5);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+
+  animation: zoom-in-zoom-out 2s infinite;
+`;
+
+const Loading = () => {
+  return (
+    <LoadingContainer>
+      <LoadingCircleContainer>L O A D I N G . . .</LoadingCircleContainer>
+    </LoadingContainer>
+  );
+};
+
+export default Loading;
